0fdd554ec8cd61f556b2472ded297ba2ab1686d9
Jeffrey Arts <sjeffff@gmail.com>
2021-03-22 20:07:30 +0100

    Basic scene ThreeJS

--- client/app/routes/dashboard/dashboard.js
+++ client/app/routes/dashboard/dashboard.js
@@ -1,12 +1,35 @@
 import angular from 'angular';
+import * as THREE from 'three';
 import routeWrap from 'ng-component-routing';
 import template from './dashboard.html';
 import './dashboard.scss';
 
-const controller = function() {
-  'ngInject';
+const controller = function($element) {
+    'ngInject';
 
-  this.name = 'dashboard';
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth*.8, window.innerHeight*.8 );
+    $element[0].appendChild( renderer.domElement );
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: "#96f" } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    function animate() {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        
+    	requestAnimationFrame( animate );
+    	renderer.render( scene, camera );
+    }
+    animate();
 };
 
 const dashboardComponent = {
